cc76b3ec8228b2296f973d0b69dac3d05a1f647c
sainsh <kfsainsh@gmail.com>
2018-10-10 15:53:25 +0200

    eight 2

---
+++ src/eight2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class eight2 {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a 4-by-4 matrix row by row:");
+
+        double[][] m = {{in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble()}, //prompt user for 4x4 matrix
+                {in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble()},
+                {in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble()},
+                {in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble()}};
+
+        System.out.printf("Sum of the elements in the major diagonal is %.2f",sumMajorDiagonal(m));
+
+    }
+
+    public static double sumMajorDiagonal(double[][] m) {
+        double sum = 0.0;
+
+        for(int i = 0; i<m.length;i++){     //sums the diagonal values
+            sum+=m[i][i];
+        }
+
+
+        return sum;
+    }
+}
